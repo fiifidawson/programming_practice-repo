b47856c4796f89064fe61a1255dab79898ccfeb4
Adlercohen <85706777+fiifidawson@users.noreply.github.com>
2023-01-02 18:57:47 +0000

    Update

--- python/OOP/OOP_2.py
+++ python/OOP/OOP_2.py
@@ -6,6 +6,7 @@
 class Student:
     #Method
     def check_pass_fail(self):
+        #self refers to the student calling it
         if self.marks >= 40:
             return True
         else: 
@@ -15,5 +16,11 @@ student1 = Student()
 student1.name = "Harry"
 student1.marks = 85
 did_pass = student1.check_pass_fail()
-
 print(did_pass)
+
+student2 = Student()
+student2.name = "Janet"
+student2.marks = 12
+did_pass = student2.check_pass_fail()
+print(did_pass)
+

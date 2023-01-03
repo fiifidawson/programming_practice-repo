9c68e69de4d503dee4071c58163b24bb3152e634
Adlercohen <85706777+fiifidawson@users.noreply.github.com>
2023-01-03 11:52:03 +0000

    Update 2_Inheritance.py

--- python/Object_Oriented_Programming/OOP_Programiz/Inheritance/2_Inheritance.py
+++ python/Object_Oriented_Programming/OOP_Programiz/Inheritance/2_Inheritance.py
@@ -1,3 +1,5 @@
+#Method Overriding:
+
 #Base clase
 class Polygon:
     def __init__(self, sides):
@@ -14,7 +16,14 @@ class Polygon:
 class Triangle(Polygon):
     def display_info(self):
         print("A triangle is a polygon with 3 edges")
+        # Preventing Method Overriding
+        # Super(): returns a temporary object of the super class for the sub class
+        super().display_info()
 
 class Quadrilateral(Polygon):
     def display_info(self):
         print("A quadrilateral is a polygon wiht four edges")
+
+t1 = Triangle([2, 4, 5])
+perimeter = t1.get_perimeter()
+print(f'The perimeter is {perimeter}')

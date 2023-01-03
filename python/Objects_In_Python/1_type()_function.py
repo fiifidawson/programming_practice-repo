a652279da83a763ef1ce91904dffd9849b149387
Adlercohen <85706777+fiifidawson@users.noreply.github.com>
2023-01-03 10:51:50 +0000

    Update

---
+++ python/Objects_In_Python/1_type()_function.py
@@ -0,0 +1,14 @@
+"""Determines the datatype of a variable"""
+
+numbers = [1, 2, 4, 1, 4, 68]
+print(type(numbers))
+
+n1 = 5
+print(type(n1))
+
+flag = True 
+print(type(flag))
+
+def function():
+    pass
+print(type(function))
---
+++ python/Objects_In_Python/3_id()_function.py
@@ -0,0 +1,11 @@
+"""The id is for identification. It is unique and constant for this object
+during its lifetime"""
+
+number1 = 5
+print(id(number1))
+
+number2 = 10
+print(id(number2))
+
+number3 = number1
+print(id(number3))
